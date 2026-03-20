912f9affcc1353f50c055b96495d16898c09d2e2
Ivan <BusinessIMS@hotmail.com>
2026-03-20 01:09:14 -0300

    add logo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import { supabase } from "@/integrations/supabase/client"
 import { useNavigate } from "react-router-dom"
-import zuninoLogo from "@/assets/Logo.jpg"
+import zuninoLogo from "@/assets/logo.jpg"
 
 export default function Login() {
   const [email, setEmail] = useState("")
